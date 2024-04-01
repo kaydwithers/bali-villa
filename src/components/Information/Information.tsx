b1824c2fc2877f317a6da996c3a446fd52a02557
kaydwithers <kaydwithers@gmail.com>
2024-04-01 21:08:27 +1100

    fix: svg jsx syntax

--- src/components/Information/Information.tsx
+++ src/components/Information/Information.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import { PlusIcon, MinusIcon } from "@heroicons/react/24/solid";
 import { Disclosure, Transition } from "@headlessui/react";
 import { Layout } from "../Layout/Layout";
 
@@ -59,35 +60,9 @@ export const Information = () => {
                           <h4 className="text-2xl">{item.heading}</h4>
 
                           {open ? (
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              stroke-width="1.5"
-                              stroke="currentColor"
-                              className="w-6 h-6"
-                            >
-                              <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="M5 12h14"
-                              />
-                            </svg>
+                            <MinusIcon className="w-6 h-6" />
                           ) : (
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              stroke-width="1.5"
-                              stroke="currentColor"
-                              className="w-6 h-6"
-                            >
-                              <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="M12 4.5v15m7.5-7.5h-15"
-                              />
-                            </svg>
+                            <PlusIcon className="w-6 h-6" />
                           )}
                         </div>
                       </Disclosure.Button>
